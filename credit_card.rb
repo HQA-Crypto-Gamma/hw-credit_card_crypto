af1df25bd671f8952b8ed999bcc15bbc97b1ab2c
Vick <vike11107@gmail.com>
2026-03-14 17:18:54 +0800

    chore: initial project setup and Luhn implementation

--- credit_card.rb
+++ credit_card.rb
@@ -1,23 +1,29 @@
 # frozen_string_literal: true
 
-require_relative './luhn_validator'
+require_relative 'luhn_validator'
 require 'json'
 
+# Create a class to represent a credit card, with the following instance variables:
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
+  include LuhnValidator
 
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
-  def to_json
+  def to_json(*)
     {
       # TODO: setup the hash with all instance vairables to serialize into json
-    }.to_json
+    }.to_json(*)
   end
 
   # returns all card information as single string
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Create a module using the Luhn Algorithm to validate credit card numbers
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -7,5 +10,13 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    nums_length = nums_a.length
+    nums_a.map!.with_index do |num, index|
+      (nums_length + index).even? ? num * 2 : num
+    end
+
+    # nums_a
+    nums_a.map! { |num| num > 9 ? num - 9 : num }
+    (nums_a.sum % 10).zero?
   end
 end
