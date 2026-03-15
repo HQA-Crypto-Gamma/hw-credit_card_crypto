21499d0ee9b2d90f2c461f99754ac7b89d566a34
linda123456789123456789 <linda109101503@g.ncu.edu.tw>
2026-03-15 10:19:30 +0800

    add: linda in hw1

--- credit_card.rb
+++ credit_card.rb
@@ -2,8 +2,8 @@
 
 require_relative 'luhn_validator'
 require 'json'
-
-# Create a class to represent a credit card, with the following instance variables:
+require 'digest'
+# This class represents a credit card with its methods to serialize, deserialize, and hash the card information.
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
   include LuhnValidator
@@ -20,14 +20,13 @@ class CreditCard
   end
 
   # returns json string
-  def to_json(*)
+  def to_json(options = {})
     {
-      # TODO: setup the hash with all instance vairables to serialize into json
       number: @number,
       expiration_date: @expiration_date,
       owner: @owner,
       credit_network: @credit_network
-    }.to_json(*)
+    }.to_json(options)
   end
 
   # returns all card information as single string
@@ -38,6 +37,13 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    data = JSON.parse(card_s)
+    CreditCard.new(
+      data['number'],
+      data['expiration_date'],
+      data['owner'],
+      data['credit_network']
+    )
   end
 
   # return a cryptographically secure hash
@@ -45,5 +51,6 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    Digest::SHA256.hexdigest("#{number}|#{expiration_date}|#{owner}|#{credit_network}")
   end
 end
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Create a module using the Luhn Algorithm to validate credit card numbers
+# This module implements the Luhn Algorithm to validate credit card numbers.
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -10,13 +10,14 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
-    nums_length = nums_a.length
-    nums_a.map!.with_index do |num, index|
-      (nums_length + index).even? ? num * 2 : num
+    sum = 0
+    nums_a.reverse.each_with_index do |num, index|
+      if index.odd?
+        num *= 2
+        num -= 9 if num > 9
+      end
+      sum += num
     end
-
-    # nums_a
-    nums_a.map! { |num| num > 9 ? num - 9 : num }
-    (nums_a.sum % 10).zero?
+    (sum % 10).zero?
   end
 end
